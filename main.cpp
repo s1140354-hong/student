69ab23b0bbd61a78124e9424ef1233de6fd7523d
s1140354-hong <s1140354@dmhs.kh.edu.tw>
2025-10-27 16:26:46 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
 int main() {
-    std::cout<<"hello,world" <<std::endl;
+    int age = 16;
+    double height = 175;
+    std::string name = "HongYi翃溢";
+    bool is_student = false;
+    std::cout << "姓名: " << name << std::endl;
+    std::cout << "身高: " << height << std::endl;
+    std::cout << "年齡: " << age << std::endl;
+    std::cout << "是否為學生: " << is_student << std::endl;
+    
+    
     return 0;
 }
