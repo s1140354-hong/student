b5b88a13a4c73f53b3178fc9a77c0ef697816eb7
s1140354-hong <s1140354@dmhs.kh.edu.tw>
2025-12-01 16:04:58 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 
-int main() {
-    int age = 16;
-    double height = 175;
-    std::string name = "HongYi翃溢";
-    bool is_student = false;
-    std::cout << "姓名: " << name << std::endl;
-    std::cout << "身高: " << height << std::endl;
-    std::cout << "年齡: " << age << std::endl;
-    std::cout << "是否為學生: " << is_student << std::endl;
-    
+int main()
+{
+    int sum;
+    for (int i=1;i<6;i++) {
+        std::cout << "現在 i 值:" << i <<std::endl;
+        sum = sum + i;
+        std::cout << "回圈內SUM 值:" << sum << std::endl;
+    }
+    std::cout << "1+..+5  總合為:" << sum;
     
     return 0;
 }
