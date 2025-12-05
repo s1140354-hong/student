379c230e508c7d91ca1219f0c7fcec49c2758a92
s1140354-hong <s1140354@dmhs.kh.edu.tw>
2025-12-05 11:41:16 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,13 +2,12 @@
 
 int main()
 {
-    int sum;
-    for (int i=1;i<6;i++) {
-        std::cout << "現在 i 值:" << i <<std::endl;
-        sum = sum + i;
-        std::cout << "回圈內SUM 值:" << sum << std::endl;
+    for(int i=1;i<=9;i++) {
+        for (int j=1;j<=9;j++) {
+            std::cout << i << "*" << j << "=" << i*j << "\t";
+        }
+        std::cout << std::endl;
     }
-    std::cout << "1+..+5  總合為:" << sum;
     
     return 0;
 }
