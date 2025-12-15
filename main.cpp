6f39e677488faad27cc9c74b738f3fa97dc93b35
s1140354-hong <s1140354@dmhs.kh.edu.tw>
2025-12-15 16:14:23 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
 
 int main()
 {
-    for(int i=1;i<=9;i++) {
-        for (int j=1;j<=9;j++) {
-            std::cout << i << "*" << j << "=" << i*j << "\t";
+     int score[5][3] = {
+        {80, 90, 85},
+        {70, 75, 78},
+        {88, 92, 95},
+        {60, 65, 70},
+        {90, 93, 89}
+    };
+    
+    string name[5] = {
+        "Amy",
+        "Ben",
+        "Cindy",
+        "David",
+        "Eva"
+    };
+    for (int i=0; i<5; i++) {
+        int sum=0;
+        cout << name[i] << " 的成績: ";
+        for (int j=0;j<3;j++) {
+            cout << score[i][j] <<  " ";
+            sum = sum + score[i][j];
         }
-        std::cout << std::endl;
+        cout<< "總分為: " << sum << endl;
     }
+    
+    
+    
+    
+    
+    
     
     return 0;
 }
